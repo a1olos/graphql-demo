f3baa8b4012ae578c6ae216e494461fbe04e9eff
th292673791 <th292673791@gmail.com>
2022-11-22 22:10:00 +0800

    init

---
+++ src/test/java/com/aiolos/graphqldemo/GraphqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiolos.graphqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
